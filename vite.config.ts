e348d68d855ff94a845a854d43e16b613db50d2a
taishinaritomi <taishinaritomi@gmail.com>
2024-06-22 21:45:47 +0900

    change workers to pages

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import pages from "@hono/vite-cloudflare-pages";
+import devServer from "@hono/vite-dev-server";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import { defineConfig } from "vite";
+
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      build: {
+        rollupOptions: {
+          input: "./src/client.ts",
+          output: {
+            entryFileNames: "static/client.js",
+          },
+        },
+      },
+    };
+  }
+
+  return {
+    ssr: {
+      external: ["react", "react-dom"],
+    },
+    plugins: [pages(), devServer({ adapter, entry: "src/index.ts" })],
+  };
+});
