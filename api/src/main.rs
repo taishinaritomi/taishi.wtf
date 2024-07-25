0bf45c65de98f72cf8f593a5f000e52eab3a2414
taishinaritomi <taishinaritomi@gmail.com>
2024-07-25 19:25:31 +0900

    fix api name

--- api/src/main.rs
+++ api/src/main.rs
@@ -15,7 +15,7 @@ async fn main() {
     let config = Config::parse();
 
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World Rust!" }));
+    let app = Router::new().route("/", get(|| async { "Taishi Naritomi" }));
 
     // run our app with hyper, listening globally on port 3000
     let addr = format!("{}:{}", "0.0.0.0", config.port);
