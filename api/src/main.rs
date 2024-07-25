c1997a37ad8cea23145c698ed76f40156569095c
taishinaritomi <taishinaritomi@gmail.com>
2024-07-25 19:19:48 +0900

    update and add rust

---
+++ api/src/main.rs
@@ -0,0 +1,24 @@
+use axum::{routing::get, Router};
+use clap::Parser;
+use dotenvy::dotenv;
+
+#[derive(Debug, Parser)]
+pub struct Config {
+    #[arg(long, env, hide_env_values = true)]
+    pub port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let config = Config::parse();
+
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World Rust!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let addr = format!("{}:{}", "0.0.0.0", config.port);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,6 @@ import { indexHandler } from "./components/Routes";
 import { checkHandler } from "./components/Routes/check";
 import { prettyJSON } from "./middlewares/pretty-json";
 import { routes } from "./routes";
-import "./utils/satori";
-import { getResvg, getSatori, getSatoriFonts } from "./utils/satori";
 
 declare module "hono" {
   interface ContextRenderer {
@@ -36,65 +34,6 @@ app.get(
 );
 
 app.get("/check", prettyJSON(), checkHandler);
-app.get("/image.svg", async (c) => {
-  const w = safeParseInt(c.req.query("w"), 1500);
-  const h = safeParseInt(c.req.query("h"), 1500);
-
-  return c.body(await getSizeSVG({ width: w, height: h }), 200, {
-    "Content-Type": "image/svg+xml",
-  });
-});
-
-app.get(
-  "/image.png",
-
-  async (c) => {
-    const Resvg = await getResvg();
-    const w = safeParseInt(c.req.query("w"), 1500);
-    const h = safeParseInt(c.req.query("h"), 1500);
-
-    const resvg = new Resvg(await getSizeSVG({ width: w, height: h }));
-
-    const pngData = resvg.render();
-    const pngBuffer = pngData.asPng();
-
-    return c.body(pngBuffer, 200, { "Content-Type": "image/png" });
-  }
-);
-
-async function getSizeSVG(size: { width: number; height: number }) {
-  const satori = await getSatori();
-
-  const svg = await satori(
-    <div
-      style={{
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100%",
-        backgroundColor: "#00ffff",
-        fontSize: 36,
-      }}
-    >
-      {size.width} x {size.height}
-    </div>,
-    {
-      width: size.width,
-      height: size.height,
-      fonts: getSatoriFonts(),
-    }
-  );
-
-  return svg;
-}
-
-function safeParseInt(value: string | undefined, defaultValue: number) {
-  try {
-    if (value) return Number.parseInt(value, 10);
-  } catch (error) {}
-  return defaultValue;
-}
 
 app.get("*", async (c) => {
   const { query, dataRoutes } = createStaticHandler(routes);
